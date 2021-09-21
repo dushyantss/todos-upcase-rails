10837eb2affb45beb9d6322a288ceceef1c0c976
Dushyant Singh Shekhawat <contact@dushyantss.com>
2021-09-21 08:33:48 +0530

    Update git history to remove bad author

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root to: 'todos#index'
+
+  resources :todos, only: [:new, :create]
+  resources :sessions, only: [:new, :create]
+end
---
+++ spec/features/user_creates_todo_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+require 'support/features/sign_in'
+
+feature 'User creates todo' do
+  include Features
+
+  scenario 'successfully' do
+    sign_in
+
+    click_on 'Add a new todo'
+    fill_in 'Title', with: 'Buy milk'
+    click_on 'Submit'
+
+    expect(page).to have_css '.todos li', text: 'Buy milk'
+  end
+end
