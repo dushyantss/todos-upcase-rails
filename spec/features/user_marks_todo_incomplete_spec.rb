ce3f3e8401127d6c761e85e4f84a489b702c6736
Dushyant Singh Shekhawat <contact@dushyantss.com>
2021-09-22 00:26:13 +0530

    Allow todos to be marked incomplete

---
+++ spec/features/user_marks_todo_incomplete_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+feature 'User marks todo incomplete' do
+  include Features
+  scenario 'successfully' do
+    sign_in
+
+    create_todo 'Buy milk'
+
+    click_on 'Mark Complete'
+    click_on 'Mark Incomplete'
+
+    expect(page).not_to display_completed_todo('Buy milk')
+    expect(page).to display_todo('Buy milk')
+  end
+end
