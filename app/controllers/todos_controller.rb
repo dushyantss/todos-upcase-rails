04027b74fac0299a38e811e9d84826f37517f900
Dushyant Singh Shekhawat <contact@dushyantss.com>
2021-09-21 09:16:44 +0530

    Refactor current_email out to current_user

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,7 +2,7 @@ class TodosController < ApplicationController
   before_action :authenticate
 
   def index
-    @todos = Todo.where(email: current_email)
+    @todos = current_user.todos
   end
 
   def new
@@ -10,13 +10,13 @@ class TodosController < ApplicationController
   end
 
   def create
-    Todo.create(todo_params)
+    current_user.todos.create(todo_params)
     redirect_to root_path
   end
 
   private
 
   def todo_params
-    params.require(:todo).permit(:title).merge(email: current_email)
+    params.require(:todo).permit(:title)
   end
 end
